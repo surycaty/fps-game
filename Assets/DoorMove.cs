26812b1f17a02ad7152ca922bb64a657a56f2c90
Adriano Surycaty <surycaty.ibg@gmail.com>
2023-09-19 21:36:14 -0300

    Initial commit

---
+++ Assets/DoorMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMove : MonoBehaviour
+{
+    public Transform m_Target;
+    public Animator m_animation;
+    public ObjectiveKillEnemies enemies = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colidiu: " + m_animation);
+        Debug.Log("Enemies: " + enemies.notificationEnemiesRemainingThreshold);
+
+        m_animation.SetBool("openDoor", true);
+    }*/
+}
